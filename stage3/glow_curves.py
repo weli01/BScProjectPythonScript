f3a392cc683b96d4c0be22412de46b1e4d8ca3f4
weli01 <147996562+weli01@users.noreply.github.com>
2024-06-28 16:27:57 +0200

    Update glow_curves.py

--- stage3/glow_curves.py
+++ stage3/glow_curves.py
@@ -7,14 +7,15 @@ MCP = pd.read_csv('data/MCP.csv', encoding='ANSI')
 MTS = pd.read_csv('data/MTS.csv', encoding='ANSI')
 
 # Define crystal names for plot
-crystal_names = ['MCP', 'MTS']
+crystal_names = [[r'MCP w/o N$_2$', r'MCP w/ N$_2$'],
+                 [r'MTS w/o N$_2$', r'MTS w/ N$_2$']]
 
 # Define mAs values and associated linestyles
 mAs = [5, 10, 20, 50, 100]
 ls = [(0, (2, 4)), '-.', ':', '--', '-']
 
 # Create a figure with two subplots sharing the same x-axis
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 7))
 
 # Iterate through crystals
 for i, crystal in enumerate([MCP, MTS]):
@@ -34,60 +35,83 @@ for i, crystal in enumerate([MCP, MTS]):
     means = []
     stdevs = []
 
-    # Iterate through mAs values
-    for ctp in mAs:
+    # Extract count data for the current mAs range
+    all_count = pd.DataFrame([crystal.iloc[[i], slice(*count_params)].squeeze().values for i in range(0, 89, 3)])
 
-        # Extract count data for the current mAs range
-        all_count = pd.DataFrame([crystal.iloc[[i], slice(*count_params)].squeeze().values for i in range(0, 89, 3)])
-        count = all_count.iloc[index+3:index+6]
+    for j in range(2):
 
-        # Compute mean and standard deviation of counts
-        mean_cnt = count.mean()
-        st_dev = count.std(axis=0)
+        if j == 0:
+            n = 0
+        else:
+            n = 3
 
-        # Append results to lists
-        means.append(mean_cnt)
-        stdevs.append(st_dev)
+        # Iterate through mAs values
+        for ctp in mAs:
 
-        index += 6
+            count = all_count.iloc[index+n:index+n+3]
+
+            # Compute mean and standard deviation of counts
+            mean_cnt = count.mean()
+            st_dev = count.std(axis=0)
+
+            # Append results to lists
+            means.append(mean_cnt)
+            stdevs.append(st_dev)
+
+            index += 6
+        index = 0
 
     # Choose the appropriate axis for the current crystal
-    ax = ax1 if i == 0 else ax2
+    axA = ax1 if i == 0 else ax3
+    axB = ax2 if i == 0 else ax4
 
-    # Plot mean counts against time
+    # Plot mean counts against time for axA and axB
     for j in range(5):
-        ax.plot(time, means[j], color='#0554f2' if i == 0 else 'darkorange', label=f'{mAs[j]} mAs', linestyle=ls[j])
-        ax.fill_between(time, means[j] - stdevs[j], means[j] + stdevs[j], color='#07bdfa' if i == 0 else 'orange', alpha=0.2)
+        axA.plot(time, means[j], color='#0554f2' if i == 0 else 'darkorange', label=f'{mAs[j]} mAs', linestyle=ls[j])
+        axA.fill_between(time, means[j] - stdevs[j], means[j] + stdevs[j], color='#07bdfa' if i == 0 else 'orange', alpha=0.2)
+        axB.plot(time, means[j + 5], color='#0554f2' if i == 0 else 'darkorange', label=f'{mAs[j]} mAs', linestyle=ls[j])
+        axB.fill_between(time, means[j + 5] - stdevs[j + 5], means[j + 5] + stdevs[j + 5], color='#07bdfa' if i == 0 else 'orange', alpha=0.2)
 
-    # Configure axis labels, limits, ticks, and formatting
-    ax.legend(loc='upper left', title='Current-time', fontsize=8.5, bbox_to_anchor=(0.001, 0.925))
-    ax.set_ylabel('Count rate [cps]')
-    ax.set_xlim(time_params[:2])  # Set x-axis limits based on time parameters
-    ax.set_ylim(0)
-    ax.minorticks_on()
-    ax.set_axisbelow(True)
-    ax.yaxis.set_major_formatter(plt.ScalarFormatter(useMathText=True))
-    ax.ticklabel_format(axis='y', style='sci', scilimits=(4, 4), useOffset=True)
+    # Common settings for axA and axB
+    for ax in [axA, axB]:
+        ax.set_xlim(time_params[:2])
+        ax.set_ylim(0, 52E4 if i == 0 else 1.6e4)
+        ax.minorticks_on()
+        ax.set_axisbelow(True)
+        ax.yaxis.set_major_formatter(plt.ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(axis='y', style='sci', scilimits=(4, 4), useOffset=True)
 
-    # Create a twin y-axis to plot temperature against time
-    ax_twin = ax.twinx()
-    ax_twin.set_ylabel(r'Temperature [$^{\circ}$C]')
-    ax_twin.plot(time, temp, color='red', linestyle='-', linewidth=0.5, alpha=0.5)
+        # Create twin y-axis for temperature against time
+        ax_twin = ax.twinx()
 
-    # Adjust limits and set maximum temperature to red
-    max_T = 240 if i == 0 else 300
-    default_ticks = [tick for tick in ax_twin.get_yticks() if tick != 300] if i == 1 else [tick for tick in ax_twin.get_yticks()]
-    ax_twin.set_yticks(default_ticks + [max_T])
-    ax_twin.set_ylim(0, 249 if i == 0 else 320)
-    ax_twin.get_yticklabels()[list(ax_twin.get_yticks()).index(max_T)].set_color('red')
-    ax_twin.tick_params(axis='y')
+        # Show temperature label and ticks only on plots on the right
+        if ax == axB:
+            ax_twin.set_ylabel(r'Temperature [$^{\circ}$C]')
+        else:
+            ax_twin.set_yticklabels([])
 
-    # Set title
-    ax.set_title(crystal_names[i], weight='bold', x=0.04, y=0.895, fontsize=14)
+        # Plot temperature against time
+        ax_twin.plot(time, temp, color='red', linestyle='-', linewidth=0.5, alpha=0.5)
+
+        # Adjust limits and set maximum temperature to red
+        max_T = 240 if i == 0 else 300
+        default_ticks = [tick for tick in ax_twin.get_yticks() if tick != 300] if i == 1 else [tick for tick in ax_twin.get_yticks()]
+        ax_twin.set_yticks(default_ticks + [max_T])
+        ax_twin.set_ylim(0, 249 if i == 0 else 320)
+        ax_twin.get_yticklabels()[list(ax_twin.get_yticks()).index(max_T)].set_color('red')
+        ax_twin.tick_params(axis='y')
+
+    # Set specific labels and titles
+    axA.legend(loc='upper left', title='Current-time', fontsize=8.5, bbox_to_anchor=(0.001, 0.925))
+    axA.set_ylabel('Count rate [cps]')
+    axA.set_title(crystal_names[i][0], weight='bold', x=0.015, y=0.90, fontsize=14, ha='left')
+    axB.set_yticklabels([])
+    axB.set_title(crystal_names[i][1], weight='bold', x=0.015, y=0.90, fontsize=14, ha='left')
 
 # Set the common x-axis label
-ax2.set_xlabel(r'Time [s]')
+ax3.set_xlabel(r'Time [s]')
+ax4.set_xlabel(r'Time [s]')
 
 # Adjust layout and save figure
-fig.subplots_adjust(top=0.98, bottom=0.03, left=0.07, right=0.99, hspace=0.17, wspace=0.1)
+fig.subplots_adjust(top=0.98, bottom=0.03, left=0.07, right=0.99, hspace=0.13, wspace=0.05)
 plt.savefig(f'results/glowcurves.pdf', bbox_inches='tight')
